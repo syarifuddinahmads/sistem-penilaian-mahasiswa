793d0dd3f4ae4e9c259cadc575846734ae702d87
Ahmad Syarifuddin <udin@gaeasystem.com>
2019-05-27 22:08:10 +0700

    sistem penilaian mahasiswa

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+struct Mahasiswa{
+    int npmMahasiswa;
+    string namaMahasiswa;
+    string jurusanMahasiswa;
+};
+
+struct User{
+    string namaUser;
+    string username;
+    string password;
+    string privilage;
+};
